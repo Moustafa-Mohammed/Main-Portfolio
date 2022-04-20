7112c47a0d0ccb6b5b6f1ac4b73c5774bfb96a5f
Mostafa Mohamed <thedevelopermostafa@gmail.com>
2022-04-20 16:03:00 +0200

    Added Links to view the projects, changed some HTML content, and applied some styles

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ const navLinks = document.querySelector(".nav-links");
 
 const navLink = document.querySelectorAll(".nav-link");
 
-console.log(navLink);
-
 hamburgerMenu.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
@@ -15,3 +13,18 @@ navLink.forEach((link) =>
     navLinks.classList.remove("open");
   })
 );
+
+const projects = document.querySelectorAll(".project");
+
+// window.addEventListener("scroll", () => {
+//   projects.forEach((p) => {
+//     const rect = p.getBoundingClientRect();
+//     if (rect.top < window.innerHeight) {
+//       p.classList.add("show-project");
+//     } else {
+//       p.classList.remove("show-project");
+//     }
+//   });
+// });
+
+window.onload = () => window.scrollTo(0, 0);
